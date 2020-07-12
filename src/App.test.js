e270d2a4e01ffccb971a1f88f37de40721c14cf6
fadlan7 <inifadlan7@gmail.com>
2020-07-12 11:28:23 +0700

    second

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import App from "./App";
+import Form from "./views/Form";
+import Redux from "./views/redux";
+import LiftingState from "./views/lifting-state";
+import Home from "./views/Home";
+import ListKeys from './views/list-key';
 
-// test('renders learn react link', () => {
+// test("renders learn react", () => {
 //   const { getByText } = render(<App />);
 //   const linkElement = getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
+
+test("renders learn react", () => {
+  const { getByText } = render(<Home />);
+  const linkElement = getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("object equality", () => {
+  const iniObject = render(<ListKeys/>);
+  iniObject["kedua"] = 2;
+  expect(iniObject).toEqual({
+    pertama: 1,
+    kedua: 2,
+  });
+});
